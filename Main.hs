1261a6bd9615a2a634834250e38e4c68b7ce6b46
Yuriy Syrovetskiy <cblp@cblp.su>
2019-03-29 17:19:18 +0300

    Make full stack project

--- Main.hs
+++ Main.hs
@@ -1,14 +1,11 @@
-#!/usr/bin/env stack
--- stack --resolver=resolver.yaml script
-
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Lens
 import           Control.Monad.IO.Class
 import           Data.Conduit
-import qualified Data.Conduit.List      as CL
-import qualified Data.Text              as T
-import qualified Data.Text.IO           as T
+import qualified Data.Conduit.List as CL
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import           Web.Twitter.Conduit
 import           Web.Twitter.Types.Lens
 
